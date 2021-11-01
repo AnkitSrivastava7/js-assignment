ee719edfb431276a7dec4cb6bc037e36c12861d4
AnkitSrivastava7 <90327449+AnkitSrivastava7@users.noreply.github.com>
2021-11-01 18:08:50 +0530

    Add files via upload
    assignment completed

---
+++ firstjsfile.js
@@ -0,0 +1,3 @@
+var myname = "Hi .. I am Ankit Srivastava";
+var num = 25;
+console.log(myname);
---
+++ thirdjsfile.js
@@ -0,0 +1,11 @@
+var a;
+console.log(a);
+//console.log(b); --> here b is not defined variable 
+
+
+a= 57;
+if(a==undefined){
+    console.log("a is undefined ");
+}else{
+    console.log("a is defined and its value is "+a);
+}
